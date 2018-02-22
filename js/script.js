2664f976e561cff236fadde88a77d2ef2b26f279
anticni <n.antic91@gmail.com>
2018-02-22 10:35:14 +0100

    added react app component that only renders a div on page

--- js/script.js
+++ js/script.js
@@ -3,15 +3,33 @@ function find() {
     let month = $('#month').val();
     let year = $('#year').val();
 
-    $.ajax({
-        type: "GET",
-        data: {
-            "api-key": "577c6c7abfb04fef9d593d84decf7d8e"
-        },
-        url: "https://api.nytimes.com/svc/archive/v1/2016/1.json",
-        success: function(data) {
-            console.log(data);
+    // $.ajax({
+    //     type: "GET",
+    //     data: {
+    //         "api-key": "577c6c7abfb04fef9d593d84decf7d8e"
+    //     },
+    //     url: "https://api.nytimes.com/svc/archive/v1/2016/1.json",
+    //     success: function(data) {
+    //         console.log(data);
 
+    //     }
+    // })
+
+    class App extends React.Component {
+
+        render() {
+
+            const text = 
+                <div>
+                hello there
+                </div>
+
+            return text;
         }
-    })
+    }
+
+    const container = document.getElementById('container');
+    
+    ReactDOM.render(<App />, container);
+
 }
